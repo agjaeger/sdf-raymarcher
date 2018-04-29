a4548e2664aebb33fae743acb1e3d9f974798949
Alex Jaeger <agjaeger@ualr.edu>
2018-04-29 17:38:55 -0500

    cool image

--- source/sdfScenes.cpp
+++ source/sdfScenes.cpp
@@ -90,5 +90,10 @@ SDF::scene (
 	glm::vec3 point,
 	SDF::Config c
 ) {
-	return boxMinusCylinderMinusCorners(point, c);
+	float scaleFactor = 0.5f;
+	return 
+		SDF::opUnion(
+			SDF::usBox(SDF::opRep(point, glm::vec3(0.5, 0.5, 0.5)), glm::vec3(0.1f, 0.1f, 0.1f)),
+			SDF::sSphere(point)
+		);
 }
