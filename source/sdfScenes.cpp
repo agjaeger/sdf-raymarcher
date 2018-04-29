c750fa8e4f5f38208e25d00a3286784f71e410be
Alex Jaeger <agjaeger@ualr.edu>
2018-04-29 17:57:54 -0500

    cool glitch image

--- source/sdfScenes.cpp
+++ source/sdfScenes.cpp
@@ -1,10 +1,8 @@
 
 #include "sdf.hpp"
 
-float
-SDF::scene (
-	glm::vec3 point,
-	SDF::Config c
+float sphereOfCubes (
+	glm::vec3 point
 ) {
 	float scaleFactor = 0.5f;
 	return 
@@ -12,4 +10,12 @@ SDF::scene (
 			SDF::usBox(SDF::opRep(point, glm::vec3(0.5, 0.5, 0.5)), glm::vec3(0.1f, 0.1f, 0.1f)),
 			SDF::sSphere(point)
 		);
+}
+
+float
+SDF::scene (
+	glm::vec3 point,
+	SDF::Config c
+) {
+	return sphereOfCubes(SDF::opRep(point, glm::vec3(2, 2, 2)));
 }
